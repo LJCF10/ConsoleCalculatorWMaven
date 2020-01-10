c621ecbfdf9c0e3edfe7848340f40cbdf3a862b4
lenny cruzinha <cruzinha.lenny@gmail.com>
2020-01-10 15:17:30 +0100

    Initialized Repository

---
+++ src/main/java/ch/bbw/_ConsoleCalculatorWMaven/App.java
@@ -0,0 +1,13 @@
+package ch.bbw._ConsoleCalculatorWMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
