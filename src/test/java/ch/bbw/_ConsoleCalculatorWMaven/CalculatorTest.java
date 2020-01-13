775c860c0295a285914700f078ecf687a48cfc17
lenny cruzinha <cruzinha.lenny@gmail.com>
2020-01-13 15:55:38 +0100

    Implemented 8 more unittests for equivalence

--- src/test/java/ch/bbw/_ConsoleCalculatorWMaven/CalculatorTest.java
+++ src/test/java/ch/bbw/_ConsoleCalculatorWMaven/CalculatorTest.java
@@ -48,5 +48,58 @@ public class CalculatorTest {
 		assertTrue(testee.division(100, 0) == 0);
 		
 	}
+	
+	@Test
+	public void testextraone() {
+		testee = new Calculator();
+		assertTrue(testee.addition(-100, 0) == -100);
+		
+	}
+	
+	@Test
+	public void testextratwo() {
+		testee = new Calculator();
+		assertTrue(testee.subtraktion(10, 0) == 10);
+		
+	}
+	
+	@Test
+	public void testextrathree() {
+		testee = new Calculator();
+		assertTrue(testee.subtraktion(-10, -10) == -20);
+		
+	}
+	
+	@Test
+	public void testextrafour() {
+		testee = new Calculator();
+		assertTrue(testee.addition(-10, -40) == -50);
+		
+	}
+	
+	@Test
+	public void testextrafive() {
+		testee = new Calculator();
+		assertTrue(testee.subtraktion(100, 100) == 0);
+		
+	}
+	
+	@Test
+	public void testextrasix() {
+		testee = new Calculator();
+		assertTrue(testee.addition(100, -100) == 0);
+	}
+	
+	@Test
+	public void testextraseven() {
+		testee = new Calculator();
+		assertTrue(testee.addition(-10, 20) == 10);	
+	}
+	
+	@Test
+	public void testextraeight() {
+		testee = new Calculator();
+		assertTrue(testee.subtraktion(-10, 20) == -30);	
+	}
 
 }
