b5afe4d283895f87737fe6e4aabc1eee36f766da
lenny cruzinha <cruzinha.lenny@gmail.com>
2020-01-19 21:03:20 +0100

    To get a better Grade, completed more tasks

--- src/test/java/ch/bbw/_ConsoleCalculatorWMaven/CalculatorTest.java
+++ src/test/java/ch/bbw/_ConsoleCalculatorWMaven/CalculatorTest.java
@@ -23,6 +23,44 @@ public class CalculatorTest {
 	}
 	
 	@Test
+	public void testaddone() {
+		assertTrue(testee.addition(-100, 0) == -100);
+		
+	}
+	
+	@Test
+	public void testaddtwo() {
+		assertTrue(testee.addition(100, -100) == 0);	
+	}
+	
+	@Test 
+	public void testMinVal() {
+		assertTrue(testee.addition(Integer.MIN_VALUE, 1)== -2147483647);
+	}
+	
+	@Test
+	public void testaddfour() {
+		assertTrue(testee.addition(-10, -40) == -50);
+		
+	}
+	
+	@Test
+	public void testaddfive() {
+		assertTrue(testee.addition(-10, 20) == 10);	
+	}
+	
+	@Test
+	public void testaddsix() {
+		assertTrue(testee.addition(10, -20) == -10);	
+	}
+	
+	@Test
+	public void testAddNegative() {
+		assertTrue(testee.addition(-100, 10) == -90);
+		
+	}
+	
+	@Test
 	public void testSubPositiv() {
 		assertTrue(testee.subtraktion(20, 10) == 10);
 		
@@ -30,15 +68,42 @@ public class CalculatorTest {
 	
 	@Test
 	public void testSubNegative() {
-		testee = new Calculator();
 		assertTrue(testee.subtraktion(10, 20) == -10);
 		
 	}
 	
+	
 	@Test
-	public void testAddNegative() {
-		assertTrue(testee.addition(-100, 10) == -90);
+	public void testsubone() {
+		assertTrue(testee.subtraktion(10, 0) == 10);
 		
+	}
+	
+	@Test
+	public void testsubtwo() {
+		assertTrue(testee.subtraktion(-10, -10) == 0);
+		
+	}
+		
+	@Test
+	public void testsubthree() {
+		assertTrue(testee.subtraktion(100, 100) == 0);
+		
+	}
+	
+	@Test
+	public void testsubfour() {
+		assertTrue(testee.subtraktion(-10, 20) == -30);	
+	}
+	
+	@Test 
+	public void testMaxVal() {
+		assertTrue(testee.subtraktion(Integer.MAX_VALUE, 1)== 2147483646);
+	}
+	
+	@Test 
+	public void testsubfive() {
+		assertTrue(testee.subtraktion(10, -10) == 20);
 	}
 	
 	//catched exception und ignoriert diese weil diese erwartete wird
@@ -61,6 +126,31 @@ public class CalculatorTest {
 		}		
 	}
 	
+	@Test
+	public void testdivone() {
+		assertTrue(testee.division(10, 2)== 5);
+	}
+	
+	@Test
+	public void testdivtwo() {
+		assertTrue(testee.division(-10, -2)== 5);
+	}
+	
+	@Test
+	public void testdivthree() {
+		assertTrue(testee.division(-10, 2)== -5);
+	}
+	
+	@Test
+	public void testdivfour() {
+		assertTrue(testee.division(10, -2)== -5);
+	}
+	
+	@Test
+	public void testdivfive() {
+		assertTrue(testee.division(10, 2)== 5);
+	}	
+	
 	//protected functions müssen im gleichen Package sein zum getestet werden
 	@Test 
 	public void testprotected(){
@@ -72,58 +162,8 @@ public class CalculatorTest {
 	// Aber wwenn man das trotzdem tun möchte, dann kann man mit Hilfe von einem PriviligedAccessor 
 	// Das ACcess Mechanisumus ändern
 	
-	@Test
-	public void testextraone() {
-		assertTrue(testee.addition(-100, 0) == -100);
-		
-	}
+
+
 	
-	@Test
-	public void testextratwo() {
-		assertTrue(testee.subtraktion(10, 0) == 10);
-		
-	}
-	
-	@Test
-	public void testextrathree() {
-		assertTrue(testee.subtraktion(-10, -10) == 0);
-		
-	}
-	
-	@Test
-	public void testextrafour() {
-		assertTrue(testee.addition(-10, -40) == -50);
-		
-	}
-	
-	@Test
-	public void testextrafive() {
-		assertTrue(testee.subtraktion(100, 100) == 0);
-		
-	}
-	
-	@Test
-	public void testextrasix() {
-		assertTrue(testee.addition(100, -100) == 0);
-	}
-	
-	@Test
-	public void testextraseven() {
-		assertTrue(testee.addition(-10, 20) == 10);	
-	}
-	
-	@Test
-	public void testextraeight() {
-		assertTrue(testee.subtraktion(-10, 20) == -30);	
-	}
-	
-	@Test 
-	public void testMaxVal() {
-		assertTrue(testee.subtraktion(Integer.MAX_VALUE, 1)== 2147483646);
-	}
-	
-	@Test 
-	public void testMinVal() {
-		assertTrue(testee.addition(Integer.MIN_VALUE, 1)== -2147483647);
-	}
+
 }
